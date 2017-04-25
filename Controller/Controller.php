b25771808e1b33f0074626f8c4de9304d8ebdbdf
mehulvp94 <mehulvp94@gmail.com>
2017-04-25 10:56:41 -0400

    Add files via upload

---
+++ Controller/Controller.php
@@ -0,0 +1,18 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+echo 'This is controller';
+
+?>
+<!DOCTYPE html>
+<html>
+    Notes: "The Controller serves as an intermediary between the Model, 
+    the View, and any other resources needed to process the HTTP request 
+    and generate a web page." 
+</html>
