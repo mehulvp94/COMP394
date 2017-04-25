0afa0ae598afbab5af2fbd95cb68bbf3f58b6f1c
mehulvp94 <mehulvp94@gmail.com>
2017-04-25 10:57:45 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+    header("Location: view/main.php");
+    exit;
+?>
+<!DOCTYPE html>
+<!--
+
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+
+        <title></title>
+    </head>
+    <body>
+    </body>
+</html>
